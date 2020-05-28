eda7666e195b23b6fda9824bf5fee3e58a2e7ff4
Zoe Arbuckle <zoe.arbuckle@me.com>
2020-05-28 15:26:47 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Zoe Arbuckle on 5/28/20.
+//  Copyright © 2020 Zoe Arbuckle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
