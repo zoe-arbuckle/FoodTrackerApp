2f361db6269a0abc7083fee578691f8da4e0bef1
Zoe Arbuckle <zoe.arbuckle@me.com>
2020-06-01 14:42:25 -0400

    connect UI to code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Handle the text field's user input through delegate callbacks. Self is the viewController.
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard, no longer the first responder
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
     
     //MARK: Actions
