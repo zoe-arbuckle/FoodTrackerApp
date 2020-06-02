1e065c6cbdc02d81fb8810174da0941b1c0f4dd2
Zoe Arbuckle <zoe.arbuckle@me.com>
2020-06-02 18:22:44 -0400

    persist data

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    //MARK: Types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     //MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int){
@@ -30,7 +42,29 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+    
+    //MARK: NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(photo, forKey: PropertyKey.photo)
+        coder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder: NSCoder) {
+        // the name is required, if we cannot decode a name string, the initialzer should fail
+        guard let name = coder.decodeObject(forKey: PropertyKey.name) as? String
+            else {
+                os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+                return nil
+        }
         
+        //because photo is optional property of Meal, just use conditional cast
+        let photo = coder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = coder.decodeInteger(forKey: PropertyKey.rating)
+        
+        self.init(name: name, photo: photo, rating: rating)
     }
     
 }
--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -20,8 +20,13 @@ class MealTableViewController: UITableViewController {
         // use the edit button item provided by the table view controller
         navigationItem.leftBarButtonItem = editButtonItem
         
-        // Load the sample data
-        loadSampleMeals()
+        //load any saved meals, otherwise load sample data
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        } else {
+            // Load the sample data
+            loadSampleMeals()
+        }
     }
 
     // MARK: - Table view data source
@@ -65,6 +70,7 @@ class MealTableViewController: UITableViewController {
         if editingStyle == .delete {
             // Delete the row from the data source
             meals.remove(at: indexPath.row)
+            saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -130,6 +136,7 @@ class MealTableViewController: UITableViewController {
                 meals.append(meal)
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
+            saveMeals()
         }
     }
     
@@ -153,6 +160,19 @@ class MealTableViewController: UITableViewController {
         }
         
         meals += [meal1, meal2, meal3]
+    }
+    
+    private func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        if (isSuccessfulSave){
+            os_log("Meals successfully saved", log: OSLog.default, type: .debug)
+        } else {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
+    }
+    
+    private func loadMeals() -> [Meal]?{
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
     }
 
 }
